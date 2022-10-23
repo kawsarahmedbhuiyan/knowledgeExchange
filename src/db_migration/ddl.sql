f9ec3ccfddc16cf584123eed58c07b621f2113b0
kawsar.bhuiyan <kawsar.bhuiyan@therapservices.net>
2022-10-23 13:04:04 +0600

    Refactored foreign key names as per code review

--- src/db_migration/ddl.sql
+++ src/db_migration/ddl.sql
@@ -27,8 +27,8 @@ CREATE TABLE user_role
     user_id int NOT NULL,
     role_id int NOT NULL,
     CONSTRAINT pk_user_role PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_user_user_role FOREIGN KEY (user_id) REFERENCES user (id),
-    CONSTRAINT fk_role_user_role FOREIGN KEY (role_id) REFERENCES role (id)
+    CONSTRAINT fk_user_role_user_id FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT fk_user_role_role_id FOREIGN KEY (role_id) REFERENCES role (id)
 );
 
 CREATE TABLE forum
@@ -41,7 +41,7 @@ CREATE TABLE forum
     updated    DATETIME    NOT NULL,
     CONSTRAINT pk_forum PRIMARY KEY (id),
     CONSTRAINT uq_forum_name UNIQUE (name),
-    CONSTRAINT fk_user_forum FOREIGN KEY (manager_id) REFERENCES user (id)
+    CONSTRAINT fk_forum_manager_id FOREIGN KEY (manager_id) REFERENCES user (id)
 );
 
 CREATE TABLE enrollment
@@ -54,8 +54,8 @@ CREATE TABLE enrollment
     updated  DATETIME    NOT NULL,
     CONSTRAINT pk_enrollment PRIMARY KEY (id),
     CONSTRAINT uq_enrollment_forum_id_user_id UNIQUE (forum_id, user_id),
-    CONSTRAINT fk_forum_enrollment FOREIGN KEY (forum_id) REFERENCES forum (id),
-    CONSTRAINT fk_user_enrollment FOREIGN KEY (user_id) REFERENCES user (id)
+    CONSTRAINT fk_enrollment_forum_id FOREIGN KEY (forum_id) REFERENCES forum (id),
+    CONSTRAINT fk_enrollment_user_id FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 CREATE TABLE post
@@ -69,8 +69,8 @@ CREATE TABLE post
     created  DATETIME      NOT NULL,
     updated  DATETIME      NOT NULL,
     CONSTRAINT pk_post PRIMARY KEY (id),
-    CONSTRAINT fk_user_post FOREIGN KEY (user_id) REFERENCES user (id),
-    CONSTRAINT fk_forum_post FOREIGN KEY (forum_id) REFERENCES forum (id)
+    CONSTRAINT fk_post_user_id FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT fk_post_forum_id FOREIGN KEY (forum_id) REFERENCES forum (id)
 );
 
 CREATE TABLE post_user_like
@@ -78,8 +78,8 @@ CREATE TABLE post_user_like
     user_id int NOT NULL,
     post_id int NOT NULL,
     CONSTRAINT pk_post_user_like PRIMARY KEY (user_id, post_id),
-    CONSTRAINT fk_user_post_user_like FOREIGN KEY (user_id) REFERENCES user (id),
-    CONSTRAINT fk_post_post_user_like FOREIGN KEY (post_id) REFERENCES post (id)
+    CONSTRAINT fk_post_user_like_user_id FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT fk_post_user_like_post_id FOREIGN KEY (post_id) REFERENCES post (id)
 );
 
 CREATE TABLE comment
@@ -92,6 +92,6 @@ CREATE TABLE comment
     created DATETIME      NOT NULL,
     updated DATETIME      NOT NULL,
     CONSTRAINT pk_comment PRIMARY KEY (id),
-    CONSTRAINT fk_user_comment FOREIGN KEY (user_id) REFERENCES user (id),
-    CONSTRAINT fk_post_comment FOREIGN KEY (post_id) REFERENCES post (id)
+    CONSTRAINT fk_comment_user_id FOREIGN KEY (user_id) REFERENCES user (id),
+    CONSTRAINT fk_comment_post_id FOREIGN KEY (post_id) REFERENCES post (id)
 );
